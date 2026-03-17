f0aadac45e3e557832e831e61b78ca262facd793
Alice Rocha <alicesouzarocha18@gmail.com>
2026-03-17 14:32:34 -0300

    organizando o eda

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -20,31 +20,84 @@ df.describe()
 # %%
 df.info()
 # %%
-## descobrir a média de horas jogadas por idade
+## 1.COMPORTAMENTO DO DATASET
 
-media_por_idade = df.groupby('age', as_index=False)['daily_gaming_hours'].mean()
+## 1.1 quem joga mais? FAIXA ETÁRIA
 
-media_por_idade.set_index('age', inplace=True)
-media_por_idade.head()
+df['faixa_idade'] = pd.cut( 
+    df['age'], 
+    bins=[0, 13, 15, 17, 20, 24, 30, 100],
+    labels=['até 13 anos', '14-15 anos', '16-17 anos', '18-20 anos', '21-24 anos', '25-30 anos', 'acima de 30 anos'] 
+)
 
-# %% 
+media_por_idade = df.groupby('faixa_idade', as_index=False)['daily_gaming_hours'].mean() 
 
-## definindo a média global
+media_por_idade.head(7)
 
-media_global = df['daily_gaming_hours'].mean()
-print(media_global)
 # %%
 ## plotar o gráfico horas jogadas x idade
 
-plt.plot(media_por_idade.index, media_por_idade.values, marker='o', color = 'green')
+plt.barh(
+    media_por_idade['faixa_idade'],          # categorias no eixo Y
+    media_por_idade['daily_gaming_hours'],   # valores no eixo X
+    color='lightgreen'
+)
 
-plt.xlabel('Idade')
-plt.axhline(media_global, color='sienna', linestyle='--', label=f'Média global: {media_global:.2f}')
-plt.ylabel('Média de horas jogadas')
-plt.title('Média de horas jogadas por idade')
-plt.legend()
+plt.xlabel('Horas jogadas')
+plt.ylabel('Faixa de idade')
+plt.title('Quais faixas etárias mais jogam?')
 
-## plt.savefig("../img/horas_jogadas_idade.png")
+for i, value in enumerate(media_por_idade['daily_gaming_hours']):
+    plt.text(value + 0.1, i, f'{value:.1f}', va='center')
+
+plt.xlim(0, max(media_por_idade['daily_gaming_hours']) + 1)
+plt.tight_layout()
+
+plt.savefig("../img/horas_jogadas_idade.png")
+
+plt.show()
+
+# %%
+
+## 1.2 quem joga mais? GÊNERO
+
+
+
+
+# %%
+
+# 2. GAMING vs saúde mental
+
+## 2.1 quem joga mais dorme menos?
+
+df['faixa_jogo'] = pd.cut(
+    df['daily_gaming_hours'],
+    bins=[0, 1, 2, 3, 5, 10],
+    labels=['0-1h', '1-2h', '2-3h', '3-5h', '5h+']
+)
+
+sono_horas_jogadas = df.groupby('faixa_jogo', as_index=False)['sleep_hours'].mean()
+
+sono_horas_jogadas.head()
+# %%
+
+## plotar o gráfico horas jogadas x sono
+
+plt.bar(
+    sono_horas_jogadas['faixa_jogo'],
+    sono_horas_jogadas['sleep_hours'],
+    color='purple'
+)
+
+plt.xlabel('Faixa de horas de jogo')
+plt.ylabel('Quantidade de horas de sono')
+plt.title('Pessoas que jogam mais dormem menos?')
+
+for i, value in enumerate(sono_horas_jogadas['sleep_hours']):
+    plt.text(i, value + 0.1, f'{value:.1f}', ha='center')
+
+plt.savefig("../img/sono_horas_jogadas.png")
 
 plt.show()
 # %%
+
